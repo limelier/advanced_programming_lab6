22ebc1b97c0bf7516f4a0439ac3e17c0a4acd973
limelier <tudor.iacobescu.1@gmail.com>
2020-03-29 23:41:12 +0300

    Added retained drawing.

---
+++ src/main/java/ui/canvas/CanvasShape.java
@@ -0,0 +1,13 @@
+package ui.canvas;
+
+import java.awt.*;
+
+class CanvasShape {
+    public Shape shape;
+    public Color color;
+
+    public CanvasShape(Shape shape, Color color) {
+        this.shape = shape;
+        this.color = color;
+    }
+}
--- src/main/java/ui/canvas/DrawingPanel.java
+++ src/main/java/ui/canvas/DrawingPanel.java
@@ -8,16 +8,26 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+
+import static util.Image.copyImage;
 
 public class DrawingPanel extends JPanel {
     final MainFrame frame;
     final static int W = 800, H = 600;
+
+    Deque<CanvasShape> activeDeque;
+    Deque<CanvasShape> redoDeque;
 
     BufferedImage image;
     Graphics2D graphics;
 
     public DrawingPanel(MainFrame frame) {
         this.frame = frame;
+        activeDeque = new ArrayDeque<>(10);
+        redoDeque = new ArrayDeque<>(10);
         createOffScreenImage();
         init();
     }
@@ -44,18 +54,39 @@ public class DrawingPanel extends JPanel {
     private void drawShape(int x, int y) {
         Shape shape = frame.shapeController.getShape(x, y);
         Color color = frame.optionsPanel.getColorIsRandom() ? Random.randomColor() : Color.BLACK;
-        graphics.setColor(color);
-        graphics.fill(shape);
+        CanvasShape canvasShape = new CanvasShape(shape, color);
+        addToUndoStack(canvasShape);
+    }
+
+    private void addToUndoStack(CanvasShape shape) {
+        if (activeDeque.size() == 50) {
+            CanvasShape lastShape = activeDeque.removeLast();
+            graphics.setColor(lastShape.color);
+            graphics.fill(shape.shape);
+        }
+        activeDeque.add(shape);
+    }
+
+    private BufferedImage activeImage() {
+        BufferedImage activeImage = copyImage(image);
+        Graphics2D g = activeImage.createGraphics();
+        for (Iterator<CanvasShape> it = activeDeque.descendingIterator(); it.hasNext(); ) {
+            CanvasShape shape = it.next();
+            g.setColor(shape.color);
+            g.fill(shape.shape);
+        }
+        g.dispose();
+        return activeImage;
     }
 
     @Override
     public void update(Graphics g) {
-        g.drawImage(image, 0, 0, this);
+        g.drawImage(activeImage(), 0, 0, this);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        g.drawImage(image, 0, 0, this);
+        g.drawImage(activeImage(), 0, 0, this);
     }
 
     public void reset() {
@@ -65,6 +96,8 @@ public class DrawingPanel extends JPanel {
     public void load(BufferedImage read) {
         image = read;
         graphics = image.createGraphics();
+        activeDeque.clear();
+        redoDeque.clear();
     }
 
     public BufferedImage getImage() {
