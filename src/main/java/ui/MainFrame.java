af592d44d1eceb0424e41f63bec84ea13f72a80a
limelier <tudor.iacobescu.1@gmail.com>
2020-03-30 00:29:44 +0300

    Added significantly more rad title.

--- src/main/java/ui/MainFrame.java
+++ src/main/java/ui/MainFrame.java
@@ -25,7 +25,7 @@ public class MainFrame extends JFrame {
     public ToolPanel toolPanel;
 
     public MainFrame() {
-        super("My Drawing Application");
+        super("Extreme Shape Drawing Experience");
         init();
     }
 
